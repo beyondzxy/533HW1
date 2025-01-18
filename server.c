64b8c949985ffb8cfdfc4d7bb49b4ea9a7f3cd05
RainZ <467706035@qq.com>
2025-01-17 19:07:26 -0800

    add function handle_client to handle each new connection, add signal to prevent zombie problem

--- server.c
+++ server.c
@@ -15,37 +15,74 @@ void error(char *msg)
     exit(1);
 }
 
+void handle_client(int newsockfd) {
+    char buffer[256];
+    int n;
+
+    bzero(buffer, 256);
+    n = read(newsockfd, buffer, 255);
+    if (n < 0) error("ERROR reading from socket");
+    printf("Here is the message: %s\n", buffer);
+
+    n = write(newsockfd, "I got your message", 18);
+    if (n < 0) error("ERROR writing to socket");
+
+    close(newsockfd);
+    exit(0);
+}
+
 int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
+     socklen_t clilen;
      struct sockaddr_in serv_addr, cli_addr;
-     int n;
+    
+     signal(SIGCHLD, SIG_IGN);
+    
      if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
+
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
         error("ERROR opening socket");
+
      bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
+
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
+
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
+
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     return 0; 
+
+     printf("Server listening on port %d...\n", portno);
+
+         while (1) {
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+        if (newsockfd < 0)
+            error("ERROR on accept");
+
+        printf("New connection accepted.\n");
+
+        pid_t pid = fork();
+        if (pid < 0) {
+            error("ERROR on fork");
+        }
+        if (pid == 0) {
+            close(sockfd);
+            handle_client(newsockfd);
+        } else {
+            close(newsockfd);
+        }
+    }
+
+    close(sockfd);
+    return 0;  
 }
